e3e9e2c59e379fc7aa6370adb9492d7b817c1f91
Finn Behrens <me@kloenk.de>
2020-06-17 17:02:00 +0200

    change to tokio

--- daemon/src/lib.rs
+++ daemon/src/lib.rs
@@ -1,21 +1,77 @@
 use std::os::unix::net::UnixStream;
+use std::sync::Arc;
+
+#[macro_use]
+extern crate log;
+use clap::{Arg, App};
 
 use error::CommandResult;
 use libutil::config::NixConfig;
 
 pub mod error;
 
-pub struct Config {
+pub struct NixDaemon {
     pub stdio: bool,
+    pub nix_config: Arc<NixConfig>,
 }
 
-impl Config {
-    pub fn new() -> Self {
-        Self::default()
+impl NixDaemon {
+    pub async fn new() -> CommandResult<Self> {
+      let mut app = App::new(env!("CARGO_PKG_NAME"))
+          .version(env!("CARGO_PKG_VERSION"))
+          .author(env!("CARGO_PKG_AUTHORS"))
+          .about(env!("CARGO_PKG_DESCRIPTION"))
+          .arg(
+              Arg::with_name("daemon")
+                  .long("daemon")
+                  .help("ignored for backwards compability")
+                  .takes_value(false),
+          )
+          .arg(
+              Arg::with_name("stdio")
+                  .long("stdio")
+                  .help("read from stdin")
+                  .takes_value(false),
+          )
+          .arg(
+              Arg::with_name("config")
+                  .long("config")
+                  .short("c")
+                  .help("set nix conifg file")
+                  .takes_value(true)
+                  .default_value("/etc/nix/nix.conf"),
+          );
+      // FIXME: add all other options
+
+      if cfg!(feature = "color") {
+          app = app
+              .setting(clap::AppSettings::ColorAuto)
+              .setting(clap::AppSettings::ColoredHelp);
+      }
+
+      let matches = app.get_matches();
+
+      let config_file = std::path::Path::new(matches.value_of("config").unwrap());
+      let nix_config = libutil::config::NixConfig::parse_file(config_file).unwrap();
+      // TODO: merge with args
+
+      let mut config = Self::new_from_config(nix_config);
+
+
+      if matches.is_present("daemon") {
+          trace!("provided `--daemon` which is only here for backward compability");
+      }
+
+      if matches.is_present("stdio") {
+          trace!("running in stdio mode");
+          config.stdio = true;
+      }
+
+      Ok(config)
     }
 
     #[allow(unused_must_use)]
-    pub fn run(self, config: &NixConfig) -> CommandResult<()> {
+    pub async fn run(self) -> CommandResult<()> {
         if self.stdio {
             // implement stdio for other store types
             let socket_path = "/nix/var/nix/daemon-socket/socket"; // FIXME: read from config
@@ -35,13 +91,13 @@ impl Config {
                 t.join().unwrap();
             }
         }
-        println!("{:#?}", config);
         Ok(()) // FIXME: unreachable() //?
     }
-}
 
-impl Default for Config {
-    fn default() -> Self {
-        Self { stdio: false }
+    fn new_from_config(config: NixConfig) -> Self {
+        Self {
+            stdio: false,
+            nix_config: Arc::new(config),
+        }
     }
 }
--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,71 +1,27 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
-use clap::{App, Arg};
+
+use nix_daemon::error::CommandResult;
+use nix_daemon::NixDaemon;
 
 fn main() {
-    /*if let Err(_) = std::env::var("RUST_LOG") {
-        // println!("set log env var");
-        std::env::set_var("RUST_LOG", "nix-daemon=trace,trace"); // TODO: change on release?
-    }*/
+    // setup env
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", "nix-daemon=trace,main=trace"); // TODO: change on release?
     }
     env_logger::init();
-    let mut app = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(
-            Arg::with_name("daemon")
-                .long("daemon")
-                .help("ignored for backwards compability")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name("stdio")
-                .long("stdio")
-                .help("read from stdin")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name("config")
-                .long("config")
-                .short("c")
-                .help("set nix conifg file")
-                .takes_value(true)
-                .default_value("/etc/nix/nix.conf"),
-        );
-    // FIXME: add all other options
 
-    if cfg!(feature = "color") {
-        app = app
-            .setting(clap::AppSettings::ColorAuto)
-            .setting(clap::AppSettings::ColoredHelp);
+    // start app
+    if let Err(e) = run() {
+        error!("{}", e);
+        std::process::exit(e.get_code());
     }
+}
 
-    let matches = app.get_matches();
-
-    let mut config = nix_daemon::Config::new();
-
-    let config_file = std::path::Path::new(matches.value_of("config").unwrap());
-    let nix_config = libutil::config::NixConfig::parse_file(config_file).unwrap();
-    // TODO: merge with args
-
-    if matches.is_present("daemon") {
-        trace!("provided `--daemon` which is only here for backward compability");
-    }
-
-    if matches.is_present("stdio") {
-        trace!("running in stdio mode");
-        config.stdio = true;
-    }
-
-    match config.run(&nix_config) {
-        Ok(_) => std::process::exit(0),
-        Err(v) => {
-            error!("{}", v);
-            std::process::exit(v.get_code()); // TODO: change exit codes
-        }
-    }
+#[tokio::main]
+async fn run() -> CommandResult<()> {
+    let daemon = NixDaemon::new().await?;
+    daemon.run().await?;
+    Ok(())
 }
